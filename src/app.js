408abe02203d5598a8d61350e2f2372c34db263a
Naveen B <dearnaveen732@gmail.com>
2025-01-16 21:42:00 +0530

    Dynamic route and query params

--- src/app.js
+++ src/app.js
@@ -2,39 +2,26 @@ const express = require("express");
 
 const app = express();
 
-// this only matches Get requests to the root URL call /user
-app.get("/user", (req, res) => {
-  console.log("user route");
+// this get the query params from the url and console log it
+// http://localhost:7777/user?name=John&age=30 
+//o/p: { name: 'John', age: '30' }
+// http://localhost:7777/user?userId=101
+//o/p: { userId: '101' }
+// app.get("/user", (req, res) => {
+//   console.log(req.query);
+//   res.send({ name: "John", age: 30 })
+// })
+
+// Dynamic route
+// this get the query params from the url and console log it
+//  http://localhost:7777/user/101/Naveen/xyzx(wen we hit this url in Postman) 
+// o/p: { userID: '101', name: 'Naveen', password: 'xyzx' }
+app.get("/user/:userID/:name/:password", (req, res) => {
+  console.log(req.params);
   res.send({ name: "John", age: 30 })
 })
-// this only matches Post requests to the root URL call /user
-app.post("/user", (req, res) => {
-  console.log("user route");
-  res.send({ name: "John", age: 30 })
-})
-
-app.delete("/user", (req, res) => {
-  console.log("user delted");
-})
-
-app.patch("/user", (req, res) => {
-  console.log("user delted");
-  res.send({ name: "John", age: 40 })
-})
-
-// this matches all requests to the root URL call /user
-app.use("/hello", (req, res) => {
-  res.send("hello")
-})
-
-app.use("/test", (req, res) => {
-  res.send("hello test")
-})
 
 
-app.use("/", (req, res) => {
-  res.send("Dashboard")
-})
 
 app.listen(7777, () => {
   console.log("server is successfully listening on port");
